8d5bd72d9be3a28c103efbdd5e944171abb15ab8
Ben Hornsby <ben.hornsby@pharmaseal.co>
2020-04-24 11:52:15 +0100

    Adds Ex-Machina dependency
    Allows creation of records easily in tests

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule OurFitnessPalApi.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ex_machina, "~> 2.4"}
     ]
   end
 
