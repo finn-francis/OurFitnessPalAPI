e2f1aab0e080c028887b52c8ff8cbe9b606db7db
Ben Hornsby <ben.hornsby@pharmaseal.co>
2020-04-30 10:29:32 +0100

    Creates functions to verify username and passwords
    How?
    - Main function to accept email and password and return a token if correct
    - Main functions calls private functions to check if email exists and verify the password provided

--- lib/our_fitness_pal_api/accounts.ex
+++ lib/our_fitness_pal_api/accounts.ex
@@ -4,8 +4,10 @@ defmodule OurFitnessPalApi.Accounts do
   """
 
   import Ecto.Query, warn: false
-  alias OurFitnessPalApi.Repo
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
 
+  alias OurFitnessPalApi.Repo
+  alias OurFitnessPalApi.Guardian
   alias OurFitnessPalApi.Accounts.User
 
   @doc """
@@ -106,5 +108,37 @@ defmodule OurFitnessPalApi.Accounts do
   """
   def change_user(%User{} = user, attrs \\ %{}) do
     User.changeset(user, attrs)
+  end
+
+  def token_sign_in(email, password) do
+    case email_password_auth(email, password) do
+      {:ok, user} ->
+        Guardian.encode_and_sign(user)
+      _ ->
+        {:error, :unauthorized}
+    end
+  end
+
+  defp email_password_auth(email, password) when is_binary(email) and is_binary(password) do
+    with {:ok, user} <- get_by_email(email),
+    do: verify_password(password, user)
+  end
+
+  defp get_by_email(email) when is_binary(email) do
+    case Repo.get_by(User, email: email) do
+      nil ->
+        dummy_checkpw()
+        {:error, :invalid_password}
+      user ->
+        {:ok, user}
+    end
+  end
+
+  defp verify_password(password, %User{} = user) when is_binary(password) do
+    if checkpw(password, user.password_hash) do
+      {:ok, user}
+    else
+      {:error, :invalid_password}
+    end
   end
 end
