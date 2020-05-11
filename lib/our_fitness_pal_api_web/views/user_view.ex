28e3e82a2778580514aa0963e8a688d050e89425
Ben Hornsby <ben.hornsby@pharmaseal.co>
2020-05-11 14:17:01 +0100

    Refactors controller method to catch errors in user creation
    - Previous method returned a 422 if there were any errors
    - Have added failure states to case statements to return errors to the user
    - Added a view to render JSON back to front-end

--- lib/our_fitness_pal_api_web/views/user_view.ex
+++ lib/our_fitness_pal_api_web/views/user_view.ex
@@ -25,4 +25,12 @@ defmodule OurFitnessPalApiWeb.UserView do
       message: message
     }
   end
+
+  def render("errors.json", %{changeset: changeset}) do
+    %{errors: translate_errors(changeset)}
+  end
+
+  def translate_errors(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
+  end
 end
