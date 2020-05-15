c0beabc7eae0a08f50c3e1b57960bca80508273f
Finn Francis <finnfrancis123@gmail.com>
2020-05-15 10:30:11 +0100

    Changed the view for session show to return the keyword 'session'
    Why?
    - This is a much more explicit naming.
    
    How?
    - Fixed the tests.

--- lib/our_fitness_pal_api_web/views/session_view.ex
+++ lib/our_fitness_pal_api_web/views/session_view.ex
@@ -7,7 +7,7 @@ defmodule OurFitnessPalApiWeb.SessionView do
   end
 
   def render("show.json", %{session: session}) do
-    %{data: render_one(session, SessionView, "session.json")}
+    %{session: render_one(session, SessionView, "session.json")}
   end
 
   def render("session.json", %{session: session}) do
